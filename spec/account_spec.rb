2ef8aedd1e6f869534028a4460a6b26828ead662
Camilla <camilla.lima000@gmail.com>
2022-02-04 19:27:22 +0000

    Add test not allow withdraw if balance is not enough

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -18,11 +18,18 @@ describe Account do
     expect(@account.account_balance).to eq 'Your balance is £100'
   end
 
-  it 'should be able to withdraw money' do
-    @account.deposit(100)
-    @account.withdraw(30)
-    expect(@account.account_balance).to eq 'Your balance is £70'
+  context "withdraw" do
+    it 'should be able to withdraw money' do
+      @account.deposit(100)
+      @account.withdraw(30)
+      expect(@account.account_balance).to eq 'Your balance is £70'
+    end
+    
+    it 'raises an error if the balance is less than the withdrawal amount' do
+      expect { @account.withdraw(100) }.to raise_error "Insufficient funds"
+    end
   end
 
+ 
 
 end
